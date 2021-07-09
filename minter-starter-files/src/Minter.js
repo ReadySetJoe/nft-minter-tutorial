e53b7894de71af242aae4a01749a74b7e58969a0
Joe <54547147+ReadySetJoe@users.noreply.github.com>
2021-07-09 14:13:49 -0400

    Initial setup

--- minter-starter-files/src/Minter.js
+++ minter-starter-files/src/Minter.js
@@ -1,4 +1,13 @@
 import { useEffect, useState } from "react";
+import {
+  connectWallet,
+  getCurrentWalletConnected,
+  mintNFT
+} from "./utils/interact.js";
+import axios from 'axios'
+const contractABI = require('./contract-abi.json')
+const abiDecoder = require('abi-decoder')
+abiDecoder.addABI(contractABI);
 
 const Minter = (props) => {
 
@@ -8,18 +17,80 @@ const Minter = (props) => {
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
   const [url, setURL] = useState("");
- 
-  useEffect(async () => { //TODO: implement
-    
-  }, []);
+  const [tokenURIs, setTokenURIs] = useState([]);
 
-  const connectWalletPressed = async () => { //TODO: implement
-   
+  useEffect(() => {
+    getWallet()
+}, []);
+
+    const getWallet = async () => {
+        const {address, status} = await getCurrentWalletConnected();
+        setWallet(address)
+        setStatus(status);
+
+        const response = await axios.get({
+            url: 'https://api-ropsten.etherscan.io/api',
+            params: {
+                module: 'account',
+                action:'tokenfttx',
+                contractaddress: '0x4c4a07f737bf57f6632b6cab089b78f62385acae',
+                address: '0xbeea8fb3e2679f591349636ec9165f9a7c42f881',
+                startblock: 0,
+                endblock: 999999999,
+                sort: 'asc',
+                apikey: 'DC51XFPUZ5NIRA2QCG9IVKRR4BK42RBS32'
+            }
+        })
+        const tokenURIs = []
+
+        for (const transaction of response.data.result) {
+            const transactionDetails = await window.ethereum.request({
+                method: 'eth_getTransactionByHash',
+                params: [transaction.hash]
+            })
+            const tokenURI = abiDecoder.decodeMethod(transactionDetails.input).params[1].value;
+            const result = (await axios.get(tokenURI)).data
+            tokenURIs.push(result)
+        }
+        setTokenURIs(tokenURIs)
+        addWalletListener();
+    }
+
+  const connectWalletPressed = async () => {
+    const walletResponse = await connectWallet();
+    setStatus(walletResponse.status);
+    setWallet(walletResponse.address);
   };
 
-  const onMintPressed = async () => { //TODO: implement
-    
-  };
+  const onMintPressed = async () => {
+    const { status } = await mintNFT(url, name, description);
+    setStatus(status);
+};
+
+  function addWalletListener() {
+    if (window.ethereum) {
+      window.ethereum.on("accountsChanged", (accounts) => {
+        if (accounts.length > 0) {
+          setWallet(accounts[0]);
+          setStatus("👆🏽 Write a message in the text-field above.");
+        } else {
+          setWallet("");
+          setStatus("🦊 Connect to Metamask using the top right button.");
+        }
+      });
+    } else {
+      setStatus(
+        <p>
+          {" "}
+          🦊{" "}
+          <a target="_blank" href={`https://metamask.io/download.html`}>
+            You must install Metamask, a virtual Ethereum wallet, in your
+            browser.
+          </a>
+        </p>
+      );
+    }
+  }
 
   return (
     <div className="Minter">
@@ -65,6 +136,12 @@ const Minter = (props) => {
       <p id="status">
         {status}
       </p>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <h1>My NFT's:</h1>
+        {tokenURIs.filter(uri => !uri.image.startsWith('https://test')).map(uri => <div><h3>{uri.name}</h3><p>Description: {uri.description}</p><img alt="An image of an NFT" src={uri.image} width="500px"/><br/><br/><br/><br/></div>)}
     </div>
   );
 };
